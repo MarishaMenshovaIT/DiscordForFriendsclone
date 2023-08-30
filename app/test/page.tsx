87f55f77857329daa26669f6e5c0b2dff2aec50b
Mary Menshova <mary1menshova@gmail.com>
2023-08-30 13:51:05 +0200

    discord_for_friends

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+// const TestPage = () => {
+//   return <div></div>;
+// };
+// export default TestPage;
+
+import React from "react";
+
+const TestPage = () => {
+  return <div>Test Page!</div>;
+};
+
+export default TestPage;
