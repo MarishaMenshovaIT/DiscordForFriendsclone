5f3eddecd1ac24ce831f4efe741b59f1f1c9cea7
Mary Menshova <mary1menshova@gmail.com>
2023-08-30 19:07:26 +0200

    Working on discord for Friends

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-purple-200 h-full">{children}</div>;
+};
+
+export default AuthLayout;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-purple-200">This is a protected router 🙅‍♀️</div>;
+}
